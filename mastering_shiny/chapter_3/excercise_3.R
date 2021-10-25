bc42e5c60655df92c0e63b6a7ce38916bb1fbe6c
Andres Felipe Insuasty Chamorro <afinsuasty@hotmail.com>
2021-10-24 20:49:46 -0500

    refactor: Realiza pequeños cambios para realizar shiny de capitulos

--- mastering_shiny/chapter_3/excercise_3.R
+++ mastering_shiny/chapter_3/excercise_3.R
@@ -5,5 +5,18 @@ var <- reactive(df[[input$var]])
 range <- reactive(range(var(), na.rm = TRUE))
 
 # Correction
-The previous names are used to calculate variance and range, so they are
-reserved names.
+
+
+ui <- fluidPage(
+  h2("Can I name a reactiveValue var and range ?"),
+  textOutput("answer")
+)
+
+server <- function(input, output, session) {
+  output$answer <- renderText(
+    paste("The previous names are used to calculate
+    variance and range, so they are reserved names.")
+  )
+}
+
+shiny::shinyApp(ui, server)
