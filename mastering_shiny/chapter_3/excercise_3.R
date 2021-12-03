95fd9384eaf090ebfe4e94d624bfc91e1dcc2bd0
Andres Felipe Insuasty Chamorro <afinsuasty@hotmail.com>
2021-12-03 01:14:09 -0500

    refactor: Convierte código a estructura modulo para chapter3

--- mastering_shiny/chapter_3/excercise_3.R
+++ mastering_shiny/chapter_3/excercise_3.R
@@ -7,16 +7,37 @@ range <- reactive(range(var(), na.rm = TRUE))
 # Correction
 
 
-ui <- fluidPage(
-  h2("Can I name a reactiveValue var and range ?"),
-  textOutput("answer")
-)
-
-server <- function(input, output, session) {
-  output$answer <- renderText(
-    paste("The previous names are used to calculate
-    variance and range, so they are reserved names.")
+chapter_3_excercise_3_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    h2("Can I name a reactiveValue var and range ?"),
+    textOutput(ns("answer"))
   )
 }
 
-shiny::shinyApp(ui, server)
+chapter_3_excercise_3_server <- function(id) {
+  ns <- NS(id)
+  moduleServer(
+    id = id,
+    module = function(input, output, session) {
+      output$answer <- renderText(
+        paste("The previous names are used to calculate
+          variance and range, so they are reserved names.")
+      )
+    }
+  )
+}
+
+# ui <- fluidPage(
+#   h2("Can I name a reactiveValue var and range ?"),
+#   textOutput("answer")
+# )
+
+# server <- function(input, output, session) {
+#   output$answer <- renderText(
+#     paste("The previous names are used to calculate
+#     variance and range, so they are reserved names.")
+#   )
+# }
+# 
+# shiny::shinyApp(ui, server)
