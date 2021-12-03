8d0870d097153eb39de5624281d06b40e6c92a46
Andres Felipe Insuasty Chamorro <afinsuasty@hotmail.com>
2021-12-03 01:12:05 -0500

    refactor: Convierte código a estructura modulo chapter1

--- mastering_shiny/chapter_1/excercise_2.R
+++ mastering_shiny/chapter_1/excercise_2.R
@@ -1,25 +1,62 @@
-library(shiny)
 
-ui <- fluidPage(
-  sliderInput("x", 
-	label = "If x is", 
-	min = 1, 
-	max = 50, 
-	value = 30
-	),
+chapter_1_excercise_2_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    sliderInput(
+      inputId = ns("x"),
+      label = "If x is",
+      min = 1,
+      max = 50,
+      value = 30
+    ),
   "then x times 5 is",
-  sliderInput("y",
-	label = "and y is",
-	min = 1,
-	max = 50,
-	value = 30),
-  textOutput("product")
-)
-
-server <- function(input, output, session) {
-  output$product <- renderText({ 
-    input$x * input$y
-  })
+  textOutput(ns("cincox")),
+  sliderInput(
+    inputId = ns("y"),
+    label = "and y is",
+    min = 1,
+    max = 50,
+    value = 30),
+  textOutput(ns("product"))
+  )
 }
 
-shinyApp(ui, server)
+chapter_1_excercise_2_server <- function(id) {
+  ns <- NS(id)
+  moduleServer(
+    id = id,
+    module = function(input, output, session) {
+      output$product <- renderText({ 
+        input$x * input$y
+      })
+      output$cincox <- renderText({ 
+        input$x * 5
+      })
+    }
+  )
+}
+
+# ui <- fluidPage(
+#   sliderInput("x", 
+#     label = "If x is", 
+#     min = 1, 
+#     max = 50, 
+#     value = 30
+#     ),
+#   "then x times 5 is",
+#   sliderInput("y",
+#     label = "and y is",
+#     min = 1,
+#     max = 50,
+#     value = 30),
+#   textOutput("product")
+# )
+
+
+# server <- function(input, output, session) {
+#   output$product <- renderText({ 
+#     input$x * input$y
+#   })
+# }
+# 
+# shinyApp(ui, server)
--- mastering_shiny/chapter_1/excercise_5.R
+++ mastering_shiny/chapter_1/excercise_5.R
@@ -1,26 +1,60 @@
-library(shiny)
-library(ggplot2)
-
+attachNamespace("ggplot2")
 datasets <- c("economics", "faithfuld", "seals")
-ui <- fluidPage(
-  selectInput("dataset", "Dataset", choices = datasets),
-  verbatimTextOutput("summary"),
-  plotOutput("plot")
-)
 
-server <- function(input, output, session) {
-  dataset <- reactive({
-    get(input$dataset, "package:ggplot2")
-  })
-  output$summary <- renderPrint({
-    summary(dataset())
-  })
-  output$plot <- renderPlot({
-    plot(dataset())
-  }, res = 96)
+chapter_1_excercise_5_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    selectInput(ns("dataset"), "Dataset", choices = datasets),
+    verbatimTextOutput(ns("summary")),
+    plotOutput(ns("plot"))
+  )
 }
 
-shinyApp(ui, server) 
+chapter_1_excercise_5_server <- function(id) {
+  ns <- NS(id)
+  moduleServer(
+    id = id,
+    module = function(input, output, session) {
+      dataset <- reactive({
+        get(input$dataset, "package:ggplot2")
+      })
+
+      output$summary <- renderPrint({
+        summary(dataset())
+      })
+
+      output$plot <- renderPlot(
+        {
+          plot(dataset())
+        },
+        res = 96
+      )
+    }
+  )
+}
+
+# ui <- fluidPage(
+#   selectInput("dataset", "Dataset", choices = datasets),
+#   verbatimTextOutput("summary"),
+#   plotOutput("plot")
+# )
+#
+# server <- function(input, output, session) {
+#   dataset <- reactive({
+#     get(input$dataset, "package:ggplot2")
+#   })
+#   output$summary <- renderPrint({
+#     summary(dataset())
+#   })
+#   output$plot <- renderPlot(
+#     {
+#       plot(dataset())
+#     },
+#     res = 96
+#   )
+# }
+#
+# shinyApp(ui, server)
 # 1. plotOutput
 # 2. output$Summary
 # 3. plot(dataset())
