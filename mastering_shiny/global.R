c85b2e3422553bd27d0c72050916e9d0c96c4d24
Andres Felipe Insuasty Chamorro <afinsuasty@hotmail.com>
2021-12-03 01:14:59 -0500

    refactor: Convierte código a estructura modulo para chapter4
    Adelanto #12, la aplicación contiene todas las aplicaciones hechas en
    los capitulos 1 al 4.

---
+++ mastering_shiny/global.R
@@ -0,0 +1,11 @@
+
+
+source("chapter_1/excercise_1.R")
+source("chapter_1/excercise_2.R")
+source("chapter_1/excercise_4.R")
+source("chapter_1/excercise_5.R")
+source("chapter_2/excercise_1.R")
+source("chapter_2/excercise_2.R")
+source("chapter_3/excercise_1.R")
+source("chapter_3/excercise_3.R")
+source("chapter_4/excercise_injuries.R")
