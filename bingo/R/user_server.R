6109d56127c6cbc63c1857e8ed6df63faba834dc
Andres Felipe Insuasty Chamorro <afinsuasty@hotmail.com>
2021-12-05 22:11:38 -0500

    feat: Add submodule bing

---
+++ bingo/R/user_server.R
@@ -0,0 +1,22 @@
+
+# User server
+generate_matrix_user <- function() {
+  card <- tibble::tibble(
+    "B" = sample(1:15, 5),
+    "I" = sample(16:30, 5),
+    "N" = sample(31:45, 5),
+    "G" = sample(46:60, 5),
+    "O" = sample(61:75, 5)
+  )
+  return(card)
+}
+
+update_card <- function(.data, .letter, number) {
+  .data <- .data %>%
+    dplyr::mutate({{ .letter }} :=
+      ifelse(
+        {{ .letter }} == {{ number }}, 0,
+        {{ .letter }}
+      ))
+  return(.data)
+}
---
+++ bingo/README.md
@@ -0,0 +1,2 @@
+# bingo_r
+Shiny app that contains a multiplayer Bingo app to play with players. Data is stored on firebase and connected through API.
